0dd999b44eb2552c6f8f298442028cced6419697
Pat Raynor <raynorpat@gmail.com>
2014-02-12 01:47:57 -0500

    Get rid of dmap code specialized for gcc

--- neo/tools/compilers/dmap/optimize.cpp
+++ neo/tools/compilers/dmap/optimize.cpp
@@ -181,11 +181,6 @@ static	void LinkEdge( optEdge_t *e ) {
 	e->v2->edges = e;
 }
 
-#ifdef __linux__
-
-optVertex_t *FindOptVertex( idDrawVert *v, optimizeGroup_t *opt );
-
-#else
 
 /*
 ================
@@ -227,7 +222,6 @@ static optVertex_t *FindOptVertex( idDrawVert *v, optimizeGroup_t *opt ) {
 	return vert;
 }
 
-#endif
 
 /*
 ================
